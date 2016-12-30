e0fa2fa503389cb976bb22425dc93fd459be5ff3
Rory Smith <rory.smith@korelogic.co.uk>
2016-12-30 12:36:56 +0000

    Fix for consistency as per screencast

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,15 @@
 </template>
 
 <script>
-  import ItemImage from './components/ItemImage.vue'
-  import ItemTitle from './components/ItemTitle.vue'
-  import ItemDescription from './components/ItemDescription.vue'
+import ItemImage from './components/ItemImage.vue'
+import ItemTitle from './components/ItemTitle.vue'
+import ItemDescription from './components/ItemDescription.vue'
 
-  export default {
-    components: {
-      ItemImage,
-      ItemTitle,
-      ItemDescription
-    }
+export default {
+  components: {
+    ItemImage,
+    ItemTitle,
+    ItemDescription
   }
-</script>
-
-<style>
-#app {
-  text-align: center;
 }
-</style>
+</script>
